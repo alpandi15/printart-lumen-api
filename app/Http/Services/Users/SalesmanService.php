4141f173c2ea9c53c6b95139e11a975fc131098d
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2019-10-11 17:03:52 +0700

    Add salesmanService

---
+++ app/Http/Services/Users/SalesmanService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Services\Users;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Http\Services\Utils\Query;
+use App\Http\Services\Utils\Increment;
+use App\Model\Accurate\SALESMAN as Model;
+
+class SalesmanService extends BaseController
+{
+  public static function getAll($query, $FIELDS = null) {
+    $model = new Model();
+    $data = Query::Paginate($model, $query, $FIELDS);
+    return $data;
+  }
+
+  public static function findById($id, $FIELDS = null) {
+    return Model::select($FIELDS ?: '*')
+    ->where('USERID', $id)->first();
+  }
+
+  public static function insert($data) {
+    $USERID = Increment::GETUSERID();
+    $insert = new Model();
+
+    $insert->USERID = $USERID;
+    $insert->USERNAME = isset($data['username']) ? $data['username'] : null;
+    $insert->USERLEVEL = isset($data['userLevel']) ? $data['userLevel'] : 0;
+    $insert->FULLNAME = isset($data['fullName']) ? $data['fullName'] : null;
+    $insert->USERPASSWORD = "-1";
+    $insert->SALESC = 1;
+    $insert->SALESR = 1;
+    $insert->SOC = 1;
+    $insert->SOE = 1;
+    $insert->SOR = 1;
+    $insert->DOC = 1;
+    $insert->DOR = 1;
+    $insert->CMC = 1;
+    $insert->CMR = 1;
+    $insert->PRINTSALESINVOICE = 1;
+    $insert->CHANGECINFO = 1;
+    $insert->CHANGESELLINGPRICE = 1;
+    $insert->CUSTOMERR = 1;
+    $insert->VENDORC = 1;
+    $insert->REPRINTINVOICE = 1;
+    $insert->SALESV = 1;
+    $insert->SALESL = 1;
+    $insert->SOV = 1;
+    $insert->SOL = 1;
+    $insert->CMV = 1;
+    $insert->DOV = 1;
+    $insert->DOL = 1;
+    $insert->save();
+
+    if ($insert) return $insert;
+    return false;
+  }
+
+  public static function update($id, $data) {
+    $update = Model::where('USERID', $id)->update([
+      "USERNAME" => isset($data['username']) ? $data['username'] : null,
+      "USERLEVEL" => isset($data['userLevel']) ? $data['userLevel'] : 0,
+      "FULLNAME" => isset($data['fullName']) ? $data['fullName'] : null,
+    ]);
+
+    if ($update) return $update;
+    return false;
+  }
+
+  public static function delete($id) {
+    $delete = Model::where('USERID', $id)->delete();
+    if ($delete) return $delete;
+    return false;
+  }
+
+  public static function count($query, $FIELDS = null) {
+    $model = new Model();
+    $count = Query::countData($model, $query, $FIELDS);
+    return $count;
+  }
+}
