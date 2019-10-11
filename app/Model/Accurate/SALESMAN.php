7186d399e0e2c129965f4dc9df39234550bb1bc4
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2019-10-11 16:49:28 +0700

    Add model SALESMAN

---
+++ app/Model/Accurate/SALESMAN.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SALESMAN extends Model
+{
+    protected $connection = 'firebird';
+    protected $table = 'SALESMAN';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $primaryKey = 'SALESMANID';
+}
