ef3008f6ad1df20c2a5891ffd6093e8188319348
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2019-10-12 10:45:37 +0700

    Update swagger info

--- app/Http/Swagger/SwaggerInfo.php
+++ app/Http/Swagger/SwaggerInfo.php
@@ -9,14 +9,18 @@ class SwaggerInfo extends BaseController
   /**
    * @SWG\Swagger(
    *     basePath="/",
-   *     schemes={"http"},
+   *     schemes={"http", "https"},
+   *     produces={"application/json", "application/xml"},
    *     @SWG\Info(
-   *         version="1.0.0",
-   *         title="API documentation Lumen",
-   *         @SWG\Contact(
-   *             email="hudaparodi@gmail.com"
-   *         ),
-   *     )
+   *         version= "1.0.0",
+   *         title= "API documentation for Printart Accurate"
+   *     ),
+   *     @SWG\SecurityScheme(
+   *         securityDefinition="Bearer",
+   *         type="apiKey",
+   *         name="Authorization",
+   *         in="header"
+   *     ),
    * )
    */
 }
